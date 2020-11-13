072f5a5344e6bce92b75cf3392546948bcd7820f
juneeda-maker <flora9511@daum.net>
2020-11-13 13:39:34 +0900

    양방향 연관 관계와 연관관계의 주인 1-기본

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -17,12 +17,30 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member();
-            member.setId(3L);
-            member.setUsername("C");
-            member.setRoleType(RoleType.GUEST);
 
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            findMember.getTeam();
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         }catch(Exception e){
